c2585b5ee7fec868e4cab1fba85a614e2f6a9dbd
PATRIARXCHIK <kirill.tumanov80@gmail.com>
2022-04-14 13:29:00 +0300

    1.1

--- tgbot.py
+++ tgbot.py
@@ -1,47 +1,44 @@
 import telebot
+from telebot import types
 import requests
 from bs4 import BeautifulSoup
 
 # GregoryyyyBot -  Айдишник
 # Gregory - Имя
-bot = telebot.TeleBot("5189417073:AAH73dnRMnHX1MTWn6zh6jJtcIRjmgmRhBI", parse_mode=None)
+
+
+bot = telebot.TeleBot("5189417073:AAH73dnRMnHX1MTWn6zh6jJtcIRjmgmRhBI")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('#TechnoCom')
+    item2 = types.KeyboardButton('#IT-fest_2022')
+    item3 = types.KeyboardButton('#IASF2022')
+    item4 = types.KeyboardButton('#ФестивальОКК')
+    item5 = types.KeyboardButton('#Нейрофест')
+    item6 = types.KeyboardButton('#НевидимыйМир')
+    item7 = types.KeyboardButton('#КонкурсНИР')
+    item8 = types.KeyboardButton('#VRARFest3D')
+
+    markup.add(item1, item2, item3, item4, item5, item6, item7, item8)
+    bot.send_message(message.from_user.id, 'Добро пожаловать!', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    list = []
-
-    commands = ['#help', '#TechnoCom', 'IT-fest_2022', '#IASF2022', 'ФестивальОКК', '#Нейрофест', '#Невидимый мир',
-                '#КонкурсНИР', '#VRARFest3D']
+    commandslist = ['#help', '#TechnoCom', 'IT-fest_2022', '#IASF2022', 'ФестивальОКК', '#Нейрофест', '#Невидимый мир',
+                    '#КонкурсНИР', '#VRARFest3D']
     if message.text == "#help":
         bot.send_message(message.from_user.id, "Вот список новостных групп: \n#TechnoCom\n#IT-fest_2022\n"
                                                "#IASF2022'\n#ФестивальОКК\n#Нейрофест\n#НевидимыйМир\n"
                                                "#КонкурсНИР\n#VRARFest3D")
 
-
-    while message.text == "#TechnoCom":
-        count = 2
-        Tcount = count % 2
-        if Tcount == 0:
-            bot.send_message(message.from_user.id,
-                             "\nВы подписались на рассылку этой группы")
-            while Tcount == 0:
-                url = 'https://vk.com/public212541280'
-                response = requests.get(url)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                quotes = soup.find_all('wall', class_="get")
-                for quote in quotes:
-                    if quote != 0 and quotes not in list[quote]:
-                        quote.append(list)
-                        bot.send_message(message.from_user.id, "Новая запись на #TechnoCom")
-                    if quote == 0:
-                        continue
-            count += 1
-        if Tcount == 1:
-            bot.send_message(message.from_user.id, 'Вы отписались от рассылки этой группы')
-            count += 1
-
-
+    if message.text == '#TechnoCom':
+        bot.send_message(message.from_user.id, '\nВы отписались от рассылки этой группы')
+    if message.text == "#TechnoCom":
+        bot.register_next_step_handler(message, TechnoCom)
     if message.text == "#IT-fest_2022":
         bot.send_message(message.from_user.id, "https://vk.com/itfest2022")
     if message.text == "#IASF2022":
@@ -56,29 +53,21 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'https://vk.com/nauchim.online')
     if message.text == '#VRARFest3D':
         bot.send_message(message.from_user.id, 'https://vk.com/nauchim.online')
-    if message.text not in commands:
+    if message.text not in commandslist:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши #help')
 
 
-bot.polling(none_stop=True, interval=1)
-global signal
+@bot.message_handler(chat_types=['text'])
+def TechnoCom(message):
+    parser = True
+    bot.send_message(message.from_user.id, "\nВы подписались на рассылку этой группы")
+    while parser == True:
+        url = 'https://vk.com/public212541280'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        quotes = soup.find_all('div', class_="wall_posts")
+        quotesList = [quotes]
+        bot.send_message(message.from_user.id, len(quotesList))
 
-""""
-def scrapering():
-    list = []
 
-    url = 'https://vk.com/public212541280'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    quotes = soup.find_all('div', class_="wall_post_text")
-
-    for quote in quotes:
-        signal = 0
-        if quote != 0 and quotes not in list[quote]:
-            quotes.append(list)
-            signal = 1
-            return signal
-        if quote == 0:
-            return
-    return signal
-"""
+bot.polling(none_stop=True, interval=0)
