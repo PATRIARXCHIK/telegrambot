eab9cd465b1f572b361a945ba4aa1e85c2088ee2
PATRIARXCHIK <kirill.tumanov80@gmail.com>
2022-05-14 14:59:27 +0300

    1.32

--- tgbot.py
+++ tgbot.py
@@ -19,11 +19,14 @@ def start(message):  # Кнопки меню
     item2 = types.KeyboardButton('#KvantNews')
 
     markup.add(item1, item2)
-    bot.send_message(message.from_user.id, 'Добро пожаловать!', reply_markup=markup)
-
+    bot.send_message(message.from_user.id, 'Добро пожаловать!\n'
+                                           'Вы используете KvantNews бота', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):  # Команды для бота
+    quest = bot.send_message(message.from_user.id, "Вы уверены что хотите подписаться на рассылку данной группы?\n"
+                                                   "Если да, то нажмите снова на эту же группу")
+
     commandslist = ['#help', '#Konkurs', '#KvantNews', '#IASF2022', 'ФестивальОКК', '#Нейрофест', '#Невидимый мир',
                     '#КонкурсНИР', '#VRARFest3D']
 
@@ -31,12 +34,12 @@ def get_text_messages(message):  # Команды для бота
         bot.send_message(message.from_user.id, "Вот список новостных групп: \n#Konkurs\n#KvantNews\n"
                                                "#IASF2022'\n#ФестивальОКК\n#Нейрофест\n#НевидимыйМир\n"
                                                "#КонкурсНИР\n#VRARFest3D")
+
     if message.text == "#Konkurs":
-        mesg = bot.send_message(message.from_user.id, '\nВы подписались на рассылку этой группы')
-        bot.callback_query_handler(Konkurs)
+        bot.register_next_step_handler(quest, Konkurs)
+
     if message.text == "#KvantNews":
-        mesg = bot.send_message(message.from_user.id, '\nВы подписались на рассылку этой группы')
-        bot.register_next_step_handler(mesg, KvantNews)
+        bot.register_next_step_handler(quest, KvantNews)
 
     if message.text not in commandslist:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши #help')
@@ -44,7 +47,12 @@ def get_text_messages(message):  # Команды для бота
 
 @bot.message_handler(chat_types=['text'])
 def Konkurs(message):  # Парсер Конкурсов
+    #stop = bot.send_message(message.from_user.id, "Вы уверены что хотите отписаться от рассылки данной группы?\n"
+    #                                               "Если да, то нажмите снова на эту же группу")
+    f = open('konkurstext.txt', 'w')
     parser = True
+    f = open('Konkurstext')
+    bot.send_message(message.from_user.id, '\nВы подписались на рассылку этой группы')
     while parser == True:
         url = 'https://kvantorium69.ru/konkurs/'
         response = requests.get(url)
@@ -52,15 +60,16 @@ def Konkurs(message):  # Парсер Конкурсов
         quotes = soup.find_all('div', class_="post-content")
         for i in quotes:
             if i not in MainList:
+
                 MainList.append(i)
-                bot.send_message(message.from_user.id, "Новый конкурс")
-    if message.text == '#Konkurs':
-        bot.send_message(message.from_user.id, '\nВы отписались от рассылки этой группы')
-        bot.message_handler(get_text_messages)
+                bot.send_message(message.from_user.id, "На сайте: https://kvantorium69.ru/konkurs/ \n"
+                                                       "Появился новый конкурс, зайдите посмотреть и поучавствовать")
+        #if message.text == '#Konkurs':
+        #   bot.register_next_step_handler(stop, get_text_messages)
 
 
 @bot.message_handler(chat_types=['text'])
-def KvantNews(message):  # Парсер IT-fest_2022
+def KvantNews(message): # Парсер KvantNews
     parser = True
     bot.send_message(message.from_user.id, "\nВы подписались на рассылку этой группы")
     while parser == True:
@@ -71,49 +80,11 @@ def KvantNews(message):  # Парсер IT-fest_2022
         for i in quotes:
             if i not in MainList:
                 MainList.append(i)
-                bot.send_message(message.from_user.id, "Новая статься с новостью")
-    if message.text == '#Konkurs':
-        bot.send_message(message.from_user.id, '\nВы отписались от рассылки этой группы')
-        bot.message_handler(get_text_messages)
-
-
-@bot.message_handler(chat_types=['text'])
-def IASF2022(message):  # Парсер IASF2022
-    parser = True
-    bot.send_message(message.from_user.id, "\nВы подписались на рассылку этой группы")
-    while parser == True:
-        url = 'https://vk.com/aerospaceproject'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        quotes = soup.find_all('div', class_="wall_posts")
-        quotesList = [quotes]
-        """bot.send_message(message.from_user.id, len(quotesList))"""
-
-
-@bot.message_handler(chat_types=['text'])
-def FestOKK(message):  # Парсер FestOKK
-    parser = True
-    bot.send_message(message.from_user.id, "\nВы подписались на рассылку этой группы")
-    while parser == True:
-        url = 'https://vk.com/okk_fest'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        quotes = soup.find_all('div', class_="wall_posts")
-        quotesList = [quotes]
-        """bot.send_message(message.from_user.id, len(quotesList))"""
-
-
-@bot.message_handler(chat_types=['text'])
-def Neuron(message):  # Парсер Neuron
-    parser = True
-    bot.send_message(message.from_user.id, "\nВы подписались на рассылку этой группы")
-    while parser == True:
-        url = 'https://vk.com/neurofest2022'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        quotes = soup.find_all('div', class_="wall_posts")
-        quotesList = [quotes]
-        """bot.send_message(message.from_user.id, len(quotesList))"""
+                bot.send_message(message.from_user.id, "На сайте: https://kvantorium69.ru/%d0%bd%d0%be%d0%b2%d0%be%d1%81%d1%82%d0%b8/ \n"
+                                                       "Появилась новая статья с новостью")
+        if message.text == '#Konkurs':
+            bot.send_message(message.from_user.id, '\nВы отписались от рассылки этой группы')
+            bot.message_handler(get_text_messages)
 
 
 bot.polling(none_stop=True, interval=0)
